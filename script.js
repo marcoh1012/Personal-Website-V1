695c4c2c29b922b274d555ebbfb8a893567f4fb4
marcoh1012 <33380761+marcoh1012@users.noreply.github.com>
2020-06-11 15:40:55 -0500

    animation

---
+++ script.js
@@ -0,0 +1,22 @@
+const $terminal = $("#introBox")
+$terminal.ready(() => { typewritter(0) })
+
+const info = ["> Hello World! ", "Im, Marco Herrera", "Im a Sotfware Engineer"]
+const $lines = [$(".code"), $(".name"), $(".subTitle")]
+
+function typewritter(i) {
+    if (i < info.length) {
+        typeString(info[i], $lines[i], 0, function() { typewritter(i + 1) })
+    }
+
+}
+
+function typeString(str, line, i, callback) {
+    if (i < str.length) {
+        line.append(str[i])
+        setTimeout(() => { typeString(str, line, i + 1, callback) }, 200)
+    } else {
+        setTimeout(callback, 500)
+    }
+
+}
