de681ca5b05b3245039f7ab9b2646457a6134f7a
Marco Herrera <marcoh1012@gmail.com>
2020-06-25 18:10:15 -0500

    fade

--- script.js
+++ script.js
@@ -19,16 +19,61 @@ anim
     });
 
 
-$(document).ready(() => {
-    setTimeout(function() {
-        $('.loading-page').hide();
-        $('header').removeAttr('hidden');
-        $('.mainInfo').removeAttr('hidden');
-    }, 3300);
-});
+$(document)
+    .ready(() => {
+        setTimeout(function() {
+            $('header')
+                .show()
+            $('.loading-page')
+                .fadeOut(2000)
+            $('nav')
+                .show()
+            $('#mainInfo')
+                .show();
+        }, 3300);
+    });
+
+
+//fade in sections
+
+$(document)
+    .ready(function() {
+
+        /* Every time the window is scrolled ... */
+        $(window)
+            .scroll(function() {
+
+                /* Check the location of each desired element */
+                $('.fade-div')
+                    .each(function(i) {
+
+                        var bottom_of_object = $(this)
+                            .position()
+                            .top + $(this)
+                            .outerHeight();
+                        var bottom_of_window = $(window)
+                            .scrollTop() + $(window)
+                            .height();
+
+                        /* If the object is completely visible in the window, fade it it */
+                        if (bottom_of_window > bottom_of_object) {
+
+                            $(this)
+                                .animate({ 'opacity': '1' }, 1500);
+
+                        }
+
+                    });
+
+            });
+
+    });
+
 
 
 // collapse nav bar when clicked on mobile
-$('.navbar-nav>li>a').on('click', function() {
-    $('.navbar-collapse').collapse('hide');
-});
+$('.navbar-nav>li>a')
+    .on('click', function() {
+        $('.navbar-collapse')
+            .collapse('hide');
+    });
