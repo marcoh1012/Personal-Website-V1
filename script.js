2f726e908c418a5181fb0988199a5c89d30b924e
marcoh1012 <33380761+marcoh1012@users.noreply.github.com>
2020-06-25 15:39:12 -0500

    loading intro

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const anim = anime.timeline({
+    loop: true,
+    direction: 'forward',
+});
+
+anim
+    .add({
+        targets: '#hexagon path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutQuart',
+        duration: 2000,
+        delay: function(el, i) { return i * 250 },
+    })
+    .add({
+        targets: '#hexagon #M',
+        duration: 1000,
+        opacity: 1,
+        easing: 'easeInOutQuart',
+    });
+
+
+$(document).ready(() => {
+    setTimeout(function() {
+        $('.loading-page').hide();
+        $('header').removeAttr('hidden');
+        $('.mainInfo').removeAttr('hidden');
+    }, 3300);
+});
+
+
 // collapse nav bar when clicked on mobile
 $('.navbar-nav>li>a').on('click', function() {
     $('.navbar-collapse').collapse('hide');
