cab160a7072348e107120067b9688cb437c8a5c0
Marco Herrera <marcoh1012@gmail.com>
2020-06-25 18:15:53 -0500

    fade height adjustment

--- script.js
+++ script.js
@@ -39,11 +39,9 @@ $(document)
 $(document)
     .ready(function() {
 
-        /* Every time the window is scrolled ... */
         $(window)
             .scroll(function() {
 
-                /* Check the location of each desired element */
                 $('.fade-div')
                     .each(function(i) {
 
@@ -55,8 +53,7 @@ $(document)
                             .scrollTop() + $(window)
                             .height();
 
-                        /* If the object is completely visible in the window, fade it it */
-                        if (bottom_of_window > bottom_of_object) {
+                        if (bottom_of_window > bottom_of_object - 600) {
 
                             $(this)
                                 .animate({ 'opacity': '1' }, 1500);
